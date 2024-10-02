65371e50d5f6f9d952189f4cb08c71d1308bd56b
James <kamaujames3705@gmail.com>
2024-10-02 12:08:44 +0300

    modified mydb

--- web/mydb.py
+++ web/mydb.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
+#connecting first without specifying the db
 dataBase = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="password",
-    database="crm"
+    user="jimmy",
+    password="Jimmy@123",
     )
 
 #prepare a cursor object
@@ -13,5 +13,19 @@ cursorObject = dataBase.cursor()
 #create a new database
 cursorObject.execute("CREATE DATABASE crm")
 
-print("Database created")
+
+dataBase.close()
+
+
+#Now reconnecting to the database created 'crm'
+
+dataBase = mysql.connector.connect(
+    host="localhost",
+    user="jimmy",
+    password="Jimmy@123",
+    database="crm"
+    )
+
+cursorObject = dataBase.cursor()
+
 

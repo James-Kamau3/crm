3664ff86eb369b70000542fa2245044a276a417c
James <kamaujames3705@gmail.com>
2024-10-04 11:09:14 +0300

    successfully handled login func

--- web/views.py
+++ web/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'web/home.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login Successful!")
+            return redirect('home')
+        else:
+            messages.error(request, "Error, Please try again!")
+            return redirect('home')
+            
+    else:
+        return render(request, 'web/home.html', {})

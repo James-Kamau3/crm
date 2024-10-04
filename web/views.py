f8e6fbf7915c499bf30214b9b15ca3df8c3ba554
James <kamaujames3705@gmail.com>
2024-10-04 22:25:51 +0300

    successfully handled login

--- web/views.py
+++ web/views.py
@@ -5,6 +5,9 @@ from django.contrib import messages
 
 # Create your views here.
 def home(request):
+    return render(request, 'web/home.html', {})
+    
+def login_user(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -15,8 +18,14 @@ def home(request):
             messages.success(request, "Login Successful!")
             return redirect('home')
         else:
-            messages.error(request, "Error, Please try again!")
-            return redirect('home')
+            messages.error(request, "Invalid Credentials!")
+            return redirect('login')
             
     else:
-        return render(request, 'web/home.html', {})
+        return render(request, 'web/login.html', {})  
+    
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Logout Successful!")
+    return redirect('login')

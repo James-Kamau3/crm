3158f91e139cddbe9c13f7c276ed43707d5ed4ee
James <kamaujames3705@gmail.com>
2024-10-05 21:40:02 +0300

    successfully handled register

--- web/views.py
+++ web/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm
 from django.contrib import messages
 
 
@@ -24,6 +25,24 @@ def login_user(request):
     else:
         return render(request, 'web/login.html', {})  
     
+    
+def register_user(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'web/register.html', {'form':form})
+
+	return render(request, 'web/register.html', {'form':form})
     
 def logout_user(request):
     logout(request)
